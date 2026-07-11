aaf8565d23ea3df29a9034d85d6a8747c17d7ca1
ysy <3200529342@qq.com>
2026-07-11 18:13:10 +0800

    taskmanagement-update

--- backend/app.py
+++ backend/app.py
@@ -210,4 +210,4 @@ if __name__ == '__main__':
     print('=' * 50)
     print()
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
